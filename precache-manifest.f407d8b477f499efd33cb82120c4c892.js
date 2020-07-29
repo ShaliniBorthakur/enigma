97b87795c8bdd8ccf5629a9ed81cec68f2ad2a39
ShaliniBorthakur <shaliniborthakur12148@gmail.com>
2020-07-29 20:58:45 +0530

    Updates

---
+++ precache-manifest.f407d8b477f499efd33cb82120c4c892.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba3d2c7871957aa8866ca8663124b9cf",
+    "url": "/enigma/index.html"
+  },
+  {
+    "revision": "e0db0efe78d2ec0ff865",
+    "url": "/enigma/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "165f2233e642ea133ed7",
+    "url": "/enigma/static/css/main.bc6c043c.chunk.css"
+  },
+  {
+    "revision": "e0db0efe78d2ec0ff865",
+    "url": "/enigma/static/js/2.45e24a3e.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/enigma/static/js/2.45e24a3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "165f2233e642ea133ed7",
+    "url": "/enigma/static/js/main.edf4636e.chunk.js"
+  },
+  {
+    "revision": "90ad73c8e45d2ccb8053",
+    "url": "/enigma/static/js/runtime-main.245e7f43.js"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/enigma/static/media/header-bg.e2d2a51c.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/enigma/static/media/map-image.f40ed5b1.png"
+  }
+]);
